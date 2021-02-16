a121472c99aaa395c114d0bc43cedd81c51b2eee
STNyame <sentwalidors@hotmail.com>
2021-02-16 16:56:34 +0100

    fisnished for launch!

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+    const page =
+    `
+    <html>
+        <title>Sentwali</title>
+        <body>
+            <h1>Why do comedians love !false?</h1>
+            <p>It's funny because it's true.</p>
+        </body>
+    </html>
+    `
+
+
+
+app.get('/', (req,res)=> {
+    res.send(page)
+})
+
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`);
+})
